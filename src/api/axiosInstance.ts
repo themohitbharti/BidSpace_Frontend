9eefb6ae60febd74c07b52ab56070026143a37b3
Mohit Bharti <mohitbharti373@gmail.com>
2025-04-16 15:22:21 +0530

    Refactor axios instance to use centralized config for API base URL

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,8 +1,9 @@
 
 import axios from "axios";
+import config from "../config/config";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
+  baseURL: config.apiBaseUrl || "http://localhost:5000/api",
   withCredentials: false, // If you're using cookies
 });
 
