80f3227e8259b9793616f745a3ec2053f0632a49
Mohit Bharti <mohitbharti373@gmail.com>
2025-08-05 16:53:24 +0530

    Add ScrollToTop component and integrate it into App; clean up main rendering structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import "./App.css";
 import { Header, Footer } from "./components/index";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import ScrollToTop from "./components/common/ScrollToTop";
 
 function App() {
   return (
     <div className="flex min-h-screen flex-col bg-transparent">
+      <ScrollToTop />
       <Header />
       <main className="flex-grow">
         <Outlet />
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
@@ -107,13 +106,12 @@ const router = createBrowserRouter([
 ]);
 
 createRoot(document.getElementById("root")!).render(
-  
-    <Provider store={store}>
-      <AuthProvider>
-        <WebSocketProvider>
-          <RouterProvider router={router} />
-          <Toaster position="bottom-right" />
-        </WebSocketProvider>
-      </AuthProvider>
-    </Provider>
+  <Provider store={store}>
+    <AuthProvider>
+      <WebSocketProvider>
+        <RouterProvider router={router} />
+        <Toaster position="bottom-right" />
+      </WebSocketProvider>
+    </AuthProvider>
+  </Provider>
 );
